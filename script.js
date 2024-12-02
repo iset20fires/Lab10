f4341da5fbcd93ea18b34358a66087cb3957ab98
iset20fires <cristinaveras0412@icloud.com>
2024-12-02 09:41:44 -0500

    update

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 function greetUser()
 {
-    document.getElementById('output').innerText = '<p> Hello! </p>'
+    document.getElementById('output').innerText = 'Hello!'
+}
+
+
+function ChangeMessage(){
+    let a = a;
+    let b = b;
+    console.log(a + b);
+    document.getElementById('message').innerText = <p> Hello, World! </p>;
+}
+
+
+function AddNumbers()
+{
+    let a = 10;
+    let b = 100;
+    console.log(a + b);
+    document.getElementById('heading-two').innerText = (a+b);
+    document.getElementById('para1').innerHTML = '<h1>This is final sum</h1>';
 }
